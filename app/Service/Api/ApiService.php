4b46e0c22a7e19365efc62f2f21f6570e727b557
nikronsakha <nikitborisov15@gmail.com.com>
2024-03-03 21:53:31 +0600

    add api services

--- app/Service/Api/ApiService.php
+++ app/Service/Api/ApiService.php
@@ -2,15 +2,26 @@
 
 namespace App\Service\Api;
 
+use App\Http\Resources\ContactResource;
+use App\Models\Contact;
+
 class ApiService
 {
-    public function store()
+    public function store($data)
     {
-
+        $create = Contact::create($data);
+        return new ContactResource($create);
     }
 
-    public function update()
+    public function update($request ,$id, $contact)
     {
 
+        $contact->name = $request['name'];
+        $contact->phone = $request['phone'];
+        $contact->category_id = $request['category_id'];
+
+        $contact->save();
+
+        return new ContactResource(Contact::findOrFail($id));
     }
 }
